9b0248e8399789fd735bf921d99a9d9a9513ad9c
DMIRTII <129100512+DimaKhasia@users.noreply.github.com>
2023-04-01 22:19:37 +0400

    Update Main.java

--- src/main/java/ru/dikhasiya/Main.java
+++ src/main/java/ru/dikhasiya/Main.java
@@ -1,7 +1,7 @@
 package ru.dikhasiya;
 
 public class Main {
-    private static string msg = "Hello DIMA!!!";
+    private static string msg = "Hello DIMA!";
     public static void main(String[] args) {
         System.out.println(msg);
     }
